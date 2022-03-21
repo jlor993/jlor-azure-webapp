587a8d78cd330e00cceed44ec221b6d120f4ee8b
jlor993 <jlor993@gmail.com>
2022-03-21 14:10:04 -0500

    Spring Initializer

---
+++ src/main/java/com/revature/jlorwebapp/JlorWebappApplication.java
@@ -0,0 +1,13 @@
+package com.revature.jlorwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlorWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlorWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/jlorwebapp/JlorWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.jlorwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlorWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
